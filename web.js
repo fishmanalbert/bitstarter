2184836fb08a4eb893ec7ef20907540f1c8cddc2
Ubuntu <ubuntu@ip-172-31-42-94.us-west-2.compute.internal>
2013-07-11 04:54:31 +0000

    Updated web.js to pull content from index.html

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+var content = fs.readFileSync('index.html', 'utf8');
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World2!');
+// });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
