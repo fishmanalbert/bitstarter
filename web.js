7a5aed8f8c4241ce19a73b38bef34b23dd51d348
Ubuntu <ubuntu@ip-172-31-42-94.us-west-2.compute.internal>
2013-07-11 05:07:14 +0000

    Another iteration on adding index.html

--- web.js
+++ web.js
@@ -4,6 +4,10 @@ var app = express.createServer(express.logger());
 
 var homepage = fs.readFileSync('index.html', 'utf8');
 
+app.get('/', function(request, response) {
+  response.send(homepage);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
